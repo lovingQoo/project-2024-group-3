bdd4746a02d7d450e1904ac77a5f9bda4513573f
xhanap <167683316+xhanap@users.noreply.github.com>
2024-05-05 14:11:17 +0800

    Delete pages/menu.py

--- pages/menu.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def menu():
-    st.sidebar.page_link("pages/homepage.py", label="Home", icon="🏠", )
-    st.sidebar.page_link("pages/1_TxtAnalysis.py", label="Analysis", icon="⛰️")
-    st.sidebar.page_link("pages/2_map.py", label="Map", icon="🌞")
-    st.sidebar.page_link("pages/3_Categorization.py", label="Categorization", icon="⛰️")
