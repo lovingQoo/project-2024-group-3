cea09dec08c1fe635d6213a95c68305df9a777f2
xhanap <167683316+xhanap@users.noreply.github.com>
2024-05-05 14:18:50 +0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -3,8 +3,8 @@ from menu import menu
 
 def menu():
     st.sidebar.page_link("pages/homepage.py", label="Home", icon="🏠", )
-    st.sidebar.page_link("pages/1_TxtAnalysis.py", label="Analysis", icon="⛰️")
-    st.sidebar.page_link("pages/2_map.py", label="Map", icon="🌞")
+    st.sidebar.page_link("pages/1_TxtAnalysis.py", label="Analysis", icon="📖")
+    st.sidebar.page_link("pages/2_map.py", label="Map", icon="🗺️")
     st.sidebar.page_link("pages/3_Categorization.py", label="Categorization", icon="⛰️")
 
 st.switch_page("pages/homepage.py")
