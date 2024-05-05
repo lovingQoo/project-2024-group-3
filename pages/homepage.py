f385cc403b6a9a7e53d2e4e08a66ea2f877c6fc2
xhanap <167683316+xhanap@users.noreply.github.com>
2024-05-05 14:10:53 +0800

    Update homepage.py

--- pages/homepage.py
+++ pages/homepage.py
@@ -1,6 +1,12 @@
 import streamlit as st
 from menu import menu
 
+def menu():
+    st.sidebar.page_link("pages/homepage.py", label="Home", icon="🏠", )
+    st.sidebar.page_link("pages/1_TxtAnalysis.py", label="Analysis", icon="⛰️")
+    st.sidebar.page_link("pages/2_map.py", label="Map", icon="🌞")
+    st.sidebar.page_link("pages/3_Categorization.py", label="Categorization", icon="⛰️")
+
 st.switch_page("pages/homepage.py")
 
 menu()
